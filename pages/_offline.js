d87116ef491a0f90bf858b23f8bc40f3fe168f12
vilepitchforks <vile.pitchforks@gmail.com>
2021-10-25 00:22:58 +0200

    Escaped character fix

--- pages/_offline.js
+++ pages/_offline.js
@@ -1,6 +1,6 @@
 // _offline.js provided by next-pwa package: https://www.npmjs.com/package/next-pwa?activeTab=readme
 const Offline = () => {
-  return <div>Looks like you're offline?</div>;
+  return <div>Someone is offline?</div>;
 };
 
 export default Offline;
